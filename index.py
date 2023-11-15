edcb962b9d22817f559d12e28e0eaef398bfd775
jod431047 <jod431047@gmail.com>
2023-11-15 22:24:42 +0100

    add tachenrecher

--- index.py
+++ index.py
@@ -2,3 +2,20 @@ nameList = ["ziad" , "Andree" , "stefan" , 7 , 8.3]
 print(nameList)                                 #طبعلي الاسماء والارقام
 print(nameList[0])                              #طبعلي القائمة واول اسم
 print(nameList[-1])                             #طبعلي القائم بلعكس ويبدا من -1
+
+
+
+x = int(input('Bitte erste zahl geben'))
+y = int(input('Bitte erste zahl geben'))              #الة حاسبة تطبع الناقص الزائد القسمة الضرب
+
+print (x+y)
+
+print (x*y)
+
+print (x-y)
+
+print (x/y)
+
+
+
+
