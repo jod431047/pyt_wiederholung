1684c4903edaa3fb30f1eac4fee940d476c65dfc
jod431047 <jod431047@gmail.com>
2023-11-16 23:50:22 +0100

    for

--- for.py
+++ for.py
@@ -21,5 +21,19 @@
 #         print(x,"*",y ,"=" ,x*y)
 
 
-x = [1,2,3,4,5,6,7,8,9]
-print(len(x))
+# x = [1,2,3,4,5,6,7,8,9]
+# print(len(x))
+
+# for x in range (1,10):
+#     for y in range (1,10):
+#         print(x,"-",y ,"=",x-y)
+
+
+# for x in range (1,10):
+#     for y in range (1,10):
+#         print (x,"/",y,"=",x/y)
+
+
+for x in range (1,10):
+    for y in range (1,10):
+        print(x,"+",y ,"=",x+y)
