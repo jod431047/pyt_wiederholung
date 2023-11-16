8391d2048116a381cf748d8dc0343144ea33c689
jod431047 <jod431047@gmail.com>
2023-11-16 22:22:21 +0100

    while.py

---
+++ while.py
@@ -0,0 +1,18 @@
+# while schleife
+
+# counter = 5                            # Anzahl der angegebenen print (ausgeführten Code)
+# while counter < 10:                    # sobald diese Bedingung true ist dann wird weiter ausgeführt
+#     print("Hier der Code, der wiederholst ausgeführt werden soll")
+#     counter = counter + 1              # Zähler
+
+# counter = 2
+# while counter < 10:
+#     print("hallo jihad")
+#     counter = counter +1
+
+x = int(input("How many times do you want to repeat your Name: "))
+y = 0
+name = input("Enter your Name: ")
+while  y < x:
+    print(name)
+    y = y + 1
