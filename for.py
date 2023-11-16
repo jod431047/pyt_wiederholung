a92cecb219a0ddeb2ec6231c416760f8839cd248
jod431047 <jod431047@gmail.com>
2023-11-16 22:50:21 +0100

    for.py

---
+++ for.py
@@ -0,0 +1,13 @@
+for x in range(89):
+    print(x)
+
+
+lag=["python","php","css","html"]
+for x in lag:
+    print(x,end=" ") # in horizontal way
+
+
+# other way
+lag=["python","php","css","html"]
+for y in range(len(lag)):
+    print(lag[y],end=" ")
