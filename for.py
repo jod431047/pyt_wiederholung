d68f24f38144cc15587c5526a9401be12503d4db
jod431047 <jod431047@gmail.com>
2023-11-16 23:44:15 +0100

    for

--- for.py
+++ for.py
@@ -1,13 +1,25 @@
-for x in range(89):
-    print(x)
+# for x in range(89):
+#     print(x)
 
 
-lag=["python","php","css","html"]
-for x in lag:
-    print(x,end=" ") # in horizontal way
+# lag=["python","php","css","html"]
+# for x in lag:
+#     print(x,end=" ") # in horizontal way
+#
+#
+# # other way
+# lag=["python","php","css","html"]
+# for y in range(len(lag)):
+#     print(lag[y],end=" ")
+
+# jihad=["remagen","bonn","brlin","hamburg"]
+# for x in range (len(jihad)):
+#     print(jihad[x],end="   ")
+
+# for x in range (1,10):                    #جدول الضرب
+#     for y in range (1,10):                #########
+#         print(x,"*",y ,"=" ,x*y)
 
 
-# other way
-lag=["python","php","css","html"]
-for y in range(len(lag)):
-    print(lag[y],end=" ")
+x = [1,2,3,4,5,6,7,8,9]
+print(len(x))
