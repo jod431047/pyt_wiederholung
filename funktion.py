4c4946549ad0eb5db98efccb23ee628d0eea56b3
jod431047 <jod431047@gmail.com>
2023-11-19 22:26:08 +0100

    funktion

--- funktion.py
+++ funktion.py
@@ -14,3 +14,26 @@
 #     return [i * j , i + j , i /j ,i -j]
 # ressum = mysum(3,2)
 # print(ressum)
+
+# eine Funktion mit Parameter: Beispeil:
+# Programm ,der gibt, welche Zahl größer ist
+# def maximum(x, y):
+#     if x > y:
+#         return x  # immer in einer Funktion muss ein Wert zurückgegeben mit return
+#     elif x == y:
+#          print("sind gleich")
+#     else:
+#         return y
+#
+# res = maximum(100, 100)  # Aufrufen des Funktion im Code
+
+# def jihad (x,y):
+#     if x > y:
+#         return x
+#     elif x ==y :
+#         print("sehr schön")
+#     else:
+#         return y
+# res = jihad(9,7)
+#
+# print(res)
