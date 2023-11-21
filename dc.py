d16bb660673e3c74f45673c09bbe7645dd860074
jod431047 <jod431047@gmail.com>
2023-11-21 21:10:48 +0100

    add dict

--- dc.py
+++ dc.py
@@ -1,11 +1,24 @@
 # dict  erstellen
+# info = {
+#     "name":"jihad",
+#     "age":33,
+#     "country":"german"
+# }
+# print(info.keys())                       # dict_keys(['name', 'age', 'country'])
+# print(info.values())                     # dict_values(['ziad', 27, 'german']) just the dict_values
+# print(info.get("age"))                   # 27 ,when the key not found than using .get(key,"message") to user
+# thisdict = dict(x=[2, 3, 4], y=[2, 3, 4], z=[2, 3, 4])
+# print(thisdict)
+
 info = {
-    "name":"jihad",
+    "name":"bari",
     "age":33,
-    "country":"german"
+    "countray":"syria",
+    "work":"developer"
 }
-print(info.keys())                       # dict_keys(['name', 'age', 'country'])
-print(info.values())                     # dict_values(['ziad', 27, 'german']) just the dict_values
-print(info.get("age"))                   # 27 ,when the key not found than using .get(key,"message") to user
-thisdict = dict(x=[2, 3, 4], y=[2, 3, 4], z=[2, 3, 4])
+
+print(info.keys())
+print(info.values())
+print(info.get("work"))
+thisdict = dict(x=[2,3,4,5],y=[5,6,7,8],j=[11,12,13,14])
 print(thisdict)
